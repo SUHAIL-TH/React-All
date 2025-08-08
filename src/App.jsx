2cb26a5bb939dc39cd364ce185ad65327bd786ed
suhail <suhailth17756@gmail.com>
2025-08-08 22:00:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import {ReducerCom} from './component/ReducerCom'
+
+function App() {
+ 
+
+  return (
+    <>
+  
+    <h1>hello suhail  </h1>
+    <ReducerCom></ReducerCom>
+  
+
+    </>
+  )
+}
+
+export default App
---
+++ src/component/ReducerCom.jsx
@@ -0,0 +1,29 @@
+//reducer implemention for counter 
+import React, { useReducer } from 'react'
+
+const initialtSate={count:0}
+function reducer(state,action){
+    switch(action.type){
+        case "increment":
+            return {count:state.count+1}
+        case "decrement":
+            return {count:state.count-1}
+        case "reset":
+            return initialtSate 
+         default :
+            throw new Error("somthing went wrong")
+
+    }
+}
+export const ReducerCom = () => {
+    const [state,dispatch]=useReducer(reducer,initialtSate)
+  return (  
+    <>
+    
+    <h1>count : {state.count}</h1>
+    <button onClick={()=>dispatch({type:"increment"})}>+</button>
+    <button onClick={()=>dispatch({type:"decrement"})}>-</button>
+    <button onClick={()=>dispatch({type:"reset"})}>reset</button>
+    </>
+  )
+}
